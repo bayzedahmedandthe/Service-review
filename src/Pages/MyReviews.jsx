a3c3c84fef85abc6c3b403be309ba758055e2de2
bayzedahmedandthe <bayzedm784@.com>
2024-12-29 18:51:21 +0600

    Complate my reviews page operation

--- src/Pages/MyReviews.jsx
+++ src/Pages/MyReviews.jsx
@@ -8,6 +8,8 @@ import Swal from "sweetalert2";
 const MyReviews = () => {
     const { user } = useContext(AuthContext);
     const [myReview, setMyReview] = useState([]);
+    const [updateValue, setUpdateValue] = useState([]);
+    const [reload, setReload] = useState(true);
 
     useEffect(() => {
         fetch(`http://localhost:5000/addReview?email=${user?.email}`)
@@ -15,7 +17,7 @@ const MyReviews = () => {
             .then(data => {
                 setMyReview(data)
             })
-    }, [user?.email]);
+    }, [user?.email, reload]);
     // console.log(myReview);
     const handleDelete = (_id) => {
         console.log(_id);
@@ -50,11 +52,51 @@ const MyReviews = () => {
         });
 
     }
+
+
+    const handleUpdate = (e) => {
+        // console.log(e);
+        // console.log(updateValue);
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const initialData = Object.fromEntries(formData.entries());
+        // console.log(updateInitialData);
+        fetch(`http://localhost:5000/addReview/${updateValue?._id}`, {
+            method: "PUT",
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify(initialData),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                if (data.modifiedCount > 0) {
+                    setReload(!reload)
+                    Swal.fire({
+                        position: "center",
+                        icon: "success",
+                        title: "Service updated successfull",
+                        showConfirmButton: false,
+                        timer: 1500,
+                    });
+                }
+            });
+
+
+
+
+
+
+
+
+        document.getElementById('my_modal_5').close()
+    }
     return (
         <div>
             <Helmet>
                 <title>My Reviews</title>
             </Helmet>
+            <h2 className="text-2xl font-semibold text-center">My reviews</h2>
             {
                 myReview.map(review => <div key={review._id} className="w-2/4 mx-auto shadow-xl rounded-lg my-6 py-6">
                     <h2 className="text-2xl font-semibold py-2 pl-4">{review.serviceTitle}</h2>
@@ -62,12 +104,59 @@ const MyReviews = () => {
                     <p className="text-gray-500 pl-4">{review.textReview}</p>
                     <div className="flex justify-end">
                         <div className="flex items-center gap-4 mt-6 mr-4">
-                            <button className="btn bg-[#00ca4c]">Update</button>
+
+                            <button onClick={() => {
+                                setUpdateValue(review)
+                                document.getElementById('my_modal_5').showModal()
+                            }} className="btn bg-[#00ca4c]">Update</button>
+
+
                             <button onClick={() => handleDelete(review._id)} className="btn text-2xl text-red-500"><RiDeleteBin6Line /></button>
                         </div>
                     </div>
                 </div>)
             }
+            <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+                <div className="modal-box">
+                    {/* form container */}
+                    <form
+                        onSubmit={handleUpdate}
+                        className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Service Title</span>
+                            </label>
+                            <input type="text" name="serviceTitle" defaultValue={updateValue.serviceTitle} placeholder="Service Title" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Service Title</span>
+                            </label>
+                            <input type="text" name="value" defaultValue={updateValue.value} placeholder="Service rating" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Description</span>
+                            </label>
+                            <textarea
+                                defaultValue={updateValue.textReview}
+                                placeholder="Description"
+                                name="textReview"
+                                className="textarea textarea-bordered textarea-md w-full "></textarea>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn bg-[#00ca4c]">Update Service</button>
+                        </div>
+                    </form>
+                    {/* form container end */}
+                    <div className="modal-action">
+                        <form method="dialog">
+                            {/* if there is a button in form, it will close the modal */}
+                            <button className="btn">Close</button>
+                        </form>
+                    </div>
+                </div>
+            </dialog>
 
         </div>
     );
