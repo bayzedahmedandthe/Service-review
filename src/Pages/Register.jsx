d13a63b2037fce4dc553a8bab31e5feb4b04ee92
bayzedahmedandthe <bayzedm784@.com>
2025-02-14 17:33:19 +0600

    Upade register page

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../Components/AuthProvider";
 import { toast } from "react-toastify";
 import { Helmet } from "react-helmet-async";
-
+import registerImg from "../assets/Filing_system-rafiki-removebg-preview.png"
 
 const Register = () => {
     const navigate = useNavigate();
@@ -46,46 +46,51 @@ const Register = () => {
     }
     return (
         <div className="bg-gradient-to-bl from-[#93c1c7] to bg-slate-500">
-            <div className="lg:w-6/12 mx-auto  pt-32">
-                <Helmet>
-                    <title>Service | Register</title>
-                </Helmet>
-                <h2 className="text-[#a1bbbf  ] text-2xl font-semibold pl-8">Register now</h2>
-                <form
-                    onSubmit={handleRegister}
-                    className="card-body">
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Name</span>
-                        </label>
-                        <input type="text" name="name" placeholder="name" className="input input-bordered" required />
-                    </div>
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Email</span>
-                        </label>
-                        <input type="email" name="email" placeholder="email" className="input input-bordered" required />
-                    </div>
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Photo-URL</span>
-                        </label>
-                        <input type="url" name="photo" placeholder="photo-URL" className="input input-bordered" required />
-                    </div>
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Password</span>
-                        </label>
-                        <input type="password" name="password" placeholder="password" className="input input-bordered" required />
-                        <label className="label">
-                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                        </label>
-                    </div>
-                    <div className="form-control mt-6">
-                        <button className="bg-[#00032e] text-gray-300 hover:text-white md:py-2 md:px-4 rounded-md">Register</button>
-                    </div>
-                    <p className="pt-3">Alredy have an account! <Link className="hover:underline" to="/login">Login</Link></p>
-                </form>
+            <div className="md:flex justify-between items-center md:max-w-7xl">
+                <div className="pt-32 flex-1">
+                    <Helmet>
+                        <title>Service | Register</title>
+                    </Helmet>
+                    <h2 className="text-[#a1bbbf  ] text-2xl font-semibold pl-8">Register now</h2>
+                    <form
+                        onSubmit={handleRegister}
+                        className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" name="name" placeholder="name" className="input input-bordered " required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Photo-URL</span>
+                            </label>
+                            <input type="url" name="photo" placeholder="photo-URL" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="bg-[#00032e] text-gray-300 hover:text-white md:py-2 md:px-4 rounded-md">Register</button>
+                        </div>
+                        <p className="pt-3">Alredy have an account! <Link className="hover:underline" to="/login">Login</Link></p>
+                    </form>
+                </div>
+                <div>
+                    <img src={registerImg} alt="" />
+                </div>
             </div>
         </div>
     );
