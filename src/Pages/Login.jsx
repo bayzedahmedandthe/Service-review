13cb45830604a60318c6f9ff50aecba5a9ca8020
bayzedahmedandthe <bayzedm784@.com>
2025-02-14 08:17:36 +0600

    Update login page

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -6,6 +6,7 @@ import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import auth from "../../Firebase.init";
 import { FcGoogle } from "react-icons/fc";
 import { Helmet } from "react-helmet-async";
+import loginImg from "../assets/3094352-removebg-preview.png"
 
 
 const Login = () => {
@@ -48,36 +49,45 @@ const Login = () => {
     }
 
     return (
-        <div className="lg:w-6/12 mx-auto md:w-8/12 pt-32">
-            <Helmet>
-                <title>Service | Login</title>
-            </Helmet>
-            <div className="flex items-center md:gap-52 gap-10 lg:gap-64 justify-center ">
-                <div>
-                    <h2 className=" font-semibold md:text-xl text-lg lg:text-2xl">Login now</h2>
+        <div className="bg-gradient-to-bl from-[#93c1c7] to bg-slate-500">
+            <div className="w-11/12 mx-auto pt-32 ">
+                <Helmet>
+                    <title>Service | Login</title>
+                </Helmet>
+                <div className="md:flex items-center justify-between">
+                    <div>
+                        <div className="flex items-center gap-12 ">
+                            <div>
+                                <h2 className=" font-semibold md:text-xl text-lg lg:text-2xl">Login now</h2>
+                            </div>
+                            <button onClick={handleLoginWithGoogle} className="bg-[#00032e] text-gray-300 hover:text-white md:py-2 md:px-4 rounded-md flex gap-4"><span className="text-2xl"><FcGoogle /></span> Login with google</button>
+                        </div>
+                        <form
+                            onSubmit={handleLoginUser}
+                            className="">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="bg-[#00032e] text-gray-300 hover:text-white md:py-2 md:px-4 rounded-md">Login</button>
+                            </div>
+                            <p className="pt-3">New to this website! <Link className="hover:underline" to="/register">register</Link></p>
+                        </form>
+                    </div>
+                    <div>
+                        <img src={loginImg} alt="" />
+                    </div>
                 </div>
-                <button onClick={handleLoginWithGoogle} className="bg-[#00032e] text-gray-300 hover:text-white md:py-2 md:px-4 rounded-md flex gap-4"><span className="text-2xl"><FcGoogle /></span> Login with google</button>
             </div>
-            <form
-                onSubmit={handleLoginUser}
-                className="card-body">
-                <div className="form-control">
-                    <label className="label">
-                        <span className="label-text">Email</span>
-                    </label>
-                    <input type="email" name="email" placeholder="email" className="input input-bordered" required />
-                </div>
-                <div className="form-control">
-                    <label className="label">
-                        <span className="label-text">Password</span>
-                    </label>
-                    <input type="password" name="password" placeholder="password" className="input input-bordered" required />
-                </div>
-                <div className="form-control mt-6">
-                    <button className="bg-[#00032e] text-gray-300 hover:text-white md:py-2 md:px-4 rounded-md">Login</button>
-                </div>
-                <p className="pt-3">New to this website! <Link className="hover:underline" to="/register">register</Link></p>
-            </form>
         </div>
     );
 };
